2f827574f7b42cba21b06f7c4aaefd54fe7f2e8b
unknown <ganeshp782791@gmail.com>
2026-09-15 23:27:35 +0530

    book project

---
+++ admin-book/src/pages/Discount/DiscountForEdit.jsx
@@ -0,0 +1,126 @@
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, Container, Row, Col, Form, Table } from "react-bootstrap";
+const apiUrl = import.meta.env.VITE_API_URL
+
+function DiscountForEdit() {
+    let navigate = useNavigate();
+    let params = useParams()
+    let id = params.id
+    let [books, setBooks] = useState([])
+    let [discount, setDiscount] = useState({
+        book: '',
+        discountName: '',
+        discountType: '',
+        discountValue : 0,
+        validFrom: '',
+        validTo: ''
+
+    })
+    
+    useEffect(() => {
+        axios({
+            url: apiUrl + '/discount/for/edit/' + id,
+            method: 'get',
+        }).then((res) => {
+            setDiscount(res.data.data)
+            setBooks(res.data.books)
+        }).catch((err) => {
+            alert(err)
+        })
+    }, [])
+
+    function manageUpdate(e) {
+        let name = e.target.name;
+        let value = e.target.value;
+        setDiscount((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    function EditDiscount() {
+        axios({
+            url: apiUrl + '/edit/discount/' + id,
+            method: 'put',
+            data: discount
+        }).then((res) => {
+            alert("Discount has been edit SuccessFully....")
+            navigate('/discounts')
+        }).catch((err) => {
+            alert("err...")
+        })
+    }
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <h3 className="text-center text-danger">Edit Discount</h3>
+                    <Form.Group>
+            <Form.Label>Select Book</Form.Label>
+            <Form.Select name="book" value={discount.book} onChange={manageUpdate}>
+              {books.map(book => (
+                <option value={book._id}>{book.bookTitle}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+                </Col>
+            </Row>
+            <Row className='mt-3'>
+        <Form.Group>
+          <Form.Label>Discount Name</Form.Label>
+          <Form.Control type='text' name="discountName" value={discount.discountName} onChange={manageUpdate}></Form.Control>
+        </Form.Group>
+      </Row>
+      <Row className='mt-3'>
+        <Form.Group>
+          <Form.Label>Discount Type</Form.Label>
+          <Form.Select name="discountType" value={discount.discountType} onChange={manageUpdate}>
+            <option value='Percentage'>Percentage</option>
+            <option value='Fixed'>Fixed</option>
+          </Form.Select>
+        </Form.Group>
+      </Row>
+      <Row className='mt-3'>
+        <Form.Group>
+          <Form.Label>Discount Value (in Number Only)</Form.Label>
+          <Form.Control type='number' name="discountValue" value={discount.discountValue} onChange={manageUpdate}></Form.Control>
+        </Form.Group>
+      </Row>
+      <Row className='mt-3'>
+        <Col>
+          <Form.Group>
+            <Form.Label>Valid From</Form.Label>
+            <Form.Control
+              type='date'
+              name="validFrom"
+              value={discount.validFrom.split('T')[0]}
+              onChange={manageUpdate}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group>
+            <Form.Label>Valid To</Form.Label>
+            <Form.Control
+              type='date'
+              name="validTo"
+              value={discount.validTo.split('T')[0]}
+              onChange={manageUpdate}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
+      <div className='text-center mt-5'>
+        <Button variant='warning' onClick={EditDiscount}>
+          Edit Discount
+        </Button>
+      </div>
+        </Container>
+    )
+}
+
+export default DiscountForEdit;
---
+++ api-book/controllers/DiscountController.js
@@ -0,0 +1,68 @@
+const Book = require('../models/Book')
+const Discount = require('../models/Discount')
+
+const getBooks = async (req, res) => {
+  try {
+    let books = await Book.find({}, { _id: 1, bookTitle: 1 })
+    // console.log(books, 'books')
+    res.status(200).send({ data: books })
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ message: 'Something Went Wrong' })
+  }
+}
+
+const addDiscount = async (req, res) => {
+  try {
+    // console.log(req.body);
+    let discount = new Discount(req.body)
+    await discount.save();
+    res.status(200).send({ message: 'Discount Added' })
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ message: 'Something Went Wrong...' })
+  }
+}
+
+const getDiscounts = async (req, res) => {
+  try {
+    let discounts = await Discount.find({}).populate('book');
+   // console.log(discounts);
+    res.status(200).send({ data: discounts});  
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: 'Something Went Wrong' })
+  }
+}
+
+const getDiscountForEdit = async (req, res) => {
+  try {
+   let id = req.params.id;
+   let discount = await Discount.findOne({_id: id});
+   let books = await Book.find({});
+   console.log(discount);
+   res.status(200).send({data: discount, books: books});
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({message: 'Something went Wrong'})
+  }
+}
+
+const editDiscount = async(req, res) => {
+  try{
+    let id = req.params.id;
+    await Discount.updateOne({_id: id}, req.body);
+    console.log("data has been update Successfully...")
+    res.status(200).send({message: 'data inserted successfully.'})
+  } catch(err) { 
+    res.status(400).send({message: "Data inserted Failed..."})
+  }
+}
+
+module.exports = {
+  getBooks,
+  addDiscount,
+  getDiscounts,
+  getDiscountForEdit,
+  editDiscount,
+};
